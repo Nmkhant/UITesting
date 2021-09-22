740c79f04c251cc5304ffa5ad055b34993d82a2f
Nyi Min Khant <85074581+Nmkhant@users.noreply.github.com>
2021-09-22 10:31:39 +0630

    Update TEST.py

--- TEST.py
+++ TEST.py
@@ -1,17 +1,9 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
-import re
-import string
-import nltk
-import warnings 
-import pickle
-from sklearn.feature_extraction.text import TfidfVectorizer
-warnings.filterwarnings("ignore", category=DeprecationWarning)
-
 
  
-st.sidebar.image("https://png.pngtree.com/template/20190323/ourmid/pngtree-a-letter-triangle-logo-image_81987.jpg" , width=300)
+st.sidebar.image("Team_Logo1.JPG" , width=300)
 
 menu = ["Home" , "About Us"]
 
@@ -19,18 +11,18 @@ choice = st.sidebar.selectbox("Menu" , menu)
 
 if choice == "Home": #Home
     
-    st.markdown("<h1 style='text-align: center; color: #ffba00'>Twitter Sentiment Analysis Tool</h1>", unsafe_allow_html=True)
+    st.markdown("<h2 style='text-align: left; color: #1DA1F2'>Twitter Sentiment Analysis Tool</h2>", unsafe_allow_html=True)
     
     st.text("")
     
-    st.image("https://freepngimg.com/thumb/twitter/8-2-twitter-png-hd.png" , width = 700)
+    st.image("Body_image.PNG" , width = 700)
     
     st.text("")
     
     user_review = st.text_input('Enter The Review You Want To Test')
     
-    if st.button("Predict"):
-        st.write('Negative')
+    if st.button("Analyze"):
+        st.write('Well Done')
 
     else:
         st.markdown("<p style= 'color:red'>Please Enter A Review</p>", unsafe_allow_html=True)
@@ -38,16 +30,16 @@ if choice == "Home": #Home
    
 else: #About Us
     
-    st.markdown("<p style='text-align: left; color: #ffba00; font-size: 150%'><b>We are Team Trio. We made the app together by doing our part task. Here, we         want to tell about ourself.</b></p>", unsafe_allow_html=True)
+    st.markdown("<p style='text-align: left; color: #1DA1F2; font-size: 150%'><b>We are Team Trio. We made the app together by doing our part task. Here, we         want to tell about ourself.</b></p>", unsafe_allow_html=True)
     
     st.text("")
     
     col1 , col2 , col3 = st.columns(3)
     col1.image('https://th.bing.com/th/id/OIP.W-P6hA0MFd0MfJUWtC025gAAAA?pid=ImgDet&rs=1', width = 200)
-    col1.write("<p style = 'text-align: left; font-size:120%; color:#ffba00'>I am Nyi Min Khant. I am a student from UTYCC. I made the User Interface of this               software.</p>", unsafe_allow_html = True)
+    col1.write("<p style = 'text-align: left; font-size:120%; color:#1DA1F2'>I am Nyi Min Khant. I am a student from UTYCC. I made the User Interface of this               software.</p>", unsafe_allow_html = True)
     
     col2.image('https://th.bing.com/th/id/OIP.W-P6hA0MFd0MfJUWtC025gAAAA?pid=ImgDet&rs=1', width = 200)
-    col2.write("<p style = 'text-align: left; font-size:120%; color:#ffba00'></p>", unsafe_allow_html = True)
+    col2.write("<p style = 'text-align: left; font-size:120%; color:#1DA1F2'></p>", unsafe_allow_html = True)
     
     col3.image('https://th.bing.com/th/id/OIP.W-P6hA0MFd0MfJUWtC025gAAAA?pid=ImgDet&rs=1', width = 200)  
-    col3.write("<p style = 'text-align: left; font-size:120%; color:#ffba00'></p>", unsafe_allow_html = True)
+    col3.write("<p style = 'text-align: left; font-size:120%; color:##1DA1F2'></p>", unsafe_allow_html = True)
