cca568726b3f7409a7bc6f57c2079e3b47e4beab
Nyi Min Khant <85074581+Nmkhant@users.noreply.github.com>
2021-09-22 17:29:03 +0630

    Update finalapp.py

--- finalapp.py
+++ finalapp.py
@@ -12,32 +12,44 @@ import re
 import pickle
 import projectfinal
 
+st.sidebar.image("Team_Logo1.JPG" , width=300)
 
+menu = ["Home" , "About Us"]
 
+choice = st.sidebar.selectbox("Menu" , menu)
+
+if choice == "Home": #Home
     
+    st.markdown("<h1 style='text-align: center; color:#1DA1F2'><b>Twitter Sentiment Analysis Tool For <br> Racism And Sexism</b></h1>", unsafe_allow_html=True)
+    
+    st.text("")
+    
+    #st.image("Body_image3.jpg" , width = 700)
+    
+    st.text("")
+    
+    user_review = st.text_input('Enter The Review You Want To Test')
+
     if st.button("Predict"):
         prediction = projectfinal.testing(user_review)
         if prediction == 0:
-            st.write('Positive')
+            st.success('Your review is a positive review. Racism and Sexism should not be in the world.You are great!')
         elif prediction == 1:
-            st.write('Negative')
-
-    else:
-        st.markdown("<p style= 'color:red'>Please Enter A Review</p>", unsafe_allow_html=True)
+            st.error('Your review is a negative review. The day you stop blaming others is the day you begin to discover who you truely are.')
 
    
 else: #About Us
     
-    st.markdown("<p style='text-align: left; color: #ffba00; font-size: 150%'><b>We are Team Trio. We made the app together by doing our part task. Here, we         want to tell about ourself.</b></p>", unsafe_allow_html=True)
+    st.markdown("<p style='text-align: left; color: #1DA1F2; font-size: 150%'><b>We are Team Trio. We made the app together by doing our part task. Here, we         want to tell about ourself.</b></p>", unsafe_allow_html=True)
     
     st.text("")
     
     col1 , col2 , col3 = st.columns(3)
     col1.image('https://th.bing.com/th/id/OIP.W-P6hA0MFd0MfJUWtC025gAAAA?pid=ImgDet&rs=1', width = 200)
-    col1.write("<p style = 'text-align: left; font-size:120%; color:#ffba00'>I am Nyi Min Khant. I am a student from UTYCC. I made the User Interface of this               software.</p>", unsafe_allow_html = True)
+    col1.write("<p style = 'text-align: left; font-size:120%; color:#1DA1F2'>I am Nyi Min Khant. I am a student from UTYCC. I made the User Interface of this               software.</p>", unsafe_allow_html = True)
     
     col2.image('https://th.bing.com/th/id/OIP.W-P6hA0MFd0MfJUWtC025gAAAA?pid=ImgDet&rs=1', width = 200)
-    col2.write("<p style = 'text-align: left; font-size:120%; color:#ffba00'></p>", unsafe_allow_html = True)
+    col2.write("<p style = 'text-align: left; font-size:120%; color:#1DA1F2'></p>", unsafe_allow_html = True)
     
     col3.image('https://th.bing.com/th/id/OIP.W-P6hA0MFd0MfJUWtC025gAAAA?pid=ImgDet&rs=1', width = 200)  
-    col3.write("<p style = 'text-align: left; font-size:120%; color:#ffba00'></p>", unsafe_allow_html = True)
+    col3.write("<p style = 'text-align: left; font-size:120%; color:#1DA1F2'></p>", unsafe_allow_html = True)
