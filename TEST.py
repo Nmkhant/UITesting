f13e82b1dceb75195f1d9d7b04ace543e0f42ef1
Nyi Min Khant <85074581+Nmkhant@users.noreply.github.com>
2021-09-22 11:06:54 +0630

    Update TEST.py

--- TEST.py
+++ TEST.py
@@ -11,7 +11,7 @@ choice = st.sidebar.selectbox("Menu" , menu)
 
 if choice == "Home": #Home
     
-    st.markdown("<h2 style='text-align: left; color: #063970'>Twitter Sentiment Analysis Tool</h2>", unsafe_allow_html=True)
+    st.markdown("<h1 style='text-align: left; color: #1DA1F2'><b>Sentiment Analysis Tool For Racist And Sexist</b></h2>", unsafe_allow_html=True)
     
     st.text("")
     
@@ -22,7 +22,7 @@ if choice == "Home": #Home
     user_review = st.text_input('Enter The Review You Want To Test')
     
     if st.button("Analyze"):
-        st.write('Well Done')
+        st.success('Well Done')
 
     else:
         st.markdown("<p style= 'color:red'>Please Enter A Review</p>", unsafe_allow_html=True)
@@ -30,16 +30,16 @@ if choice == "Home": #Home
    
 else: #About Us
     
-    st.markdown("<p style='text-align: left; color: #063970; font-size: 150%'><b>We are Team Trio. We made the app together by doing our part task. Here, we         want to tell about ourself.</b></p>", unsafe_allow_html=True)
+    st.markdown("<p style='text-align: left; color: #1DA1F2; font-size: 150%'><b>We are Team Trio. We made the app together by doing our part task. Here, we         want to tell about ourself.</b></p>", unsafe_allow_html=True)
     
     st.text("")
     
     col1 , col2 , col3 = st.columns(3)
     col1.image('https://th.bing.com/th/id/OIP.W-P6hA0MFd0MfJUWtC025gAAAA?pid=ImgDet&rs=1', width = 200)
-    col1.write("<p style = 'text-align: left; font-size:120%; color:#063970'>I am Ma Htet Htet Mon.</p>", unsafe_allow_html = True)
+    col1.write("<p style = 'text-align: left; font-size:120%; color:#1DA1F2'>I am Ma Htet Htet Mon.</p>", unsafe_allow_html = True)
     
     col2.image('https://th.bing.com/th/id/OIP.W-P6hA0MFd0MfJUWtC025gAAAA?pid=ImgDet&rs=1', width = 200)
-    col2.write("<p style = 'text-align: left; font-size:120%; color:#063970'>I am Mg Nyi Min Khant. I am a student from UTYCC. I made the User Interface of this               software</p>", unsafe_allow_html = True)
+    col2.write("<p style = 'text-align: left; font-size:120%; color:#1DA1F2'>I am Mg Nyi Min Khant. I am a student from UTYCC. I made the User Interface of this               software</p>", unsafe_allow_html = True)
     
     col3.image('https://th.bing.com/th/id/OIP.W-P6hA0MFd0MfJUWtC025gAAAA?pid=ImgDet&rs=1', width = 200)  
-    col3.write("<p style = 'text-align: left; font-size:120%; color:#063970'>I am Ma Thwet Yin Nyein.</p>", unsafe_allow_html = True)
+    col3.write("<p style = 'text-align: left; font-size:120%; color:#1DA1F2'>I am Ma Thwet Yin Nyein.</p>", unsafe_allow_html = True)
